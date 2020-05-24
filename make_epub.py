bb5062b608ec0ed92e2b2dc6da014f9309d72034
Paweł Słowik <pawel.slowik@zoho.com>
2020-05-24 15:49:10 +0200

    Blackify quotes

--- make_epub.py
+++ make_epub.py
@@ -74,7 +74,7 @@ def book_from_tar_archive(archive: bytes) -> Book:
     return Book(metadata, tuple(chapters))
 
 def remove_html_toc_references(html: str, toc_filename: str) -> str:
-    soup = bs4.BeautifulSoup(html, 'lxml')
+    soup = bs4.BeautifulSoup(html, "lxml")
     toc_links = soup.find_all("a", {"href": toc_filename})
     for toc_link in toc_links:
         if toc_link.parent.name == "li":
@@ -84,15 +84,15 @@ def remove_html_toc_references(html: str, toc_filename: str) -> str:
     return str(soup)
 
 def create_ncx(book: Book, uuid: str) -> Tuple[str, str]:
-    soup = bs4.BeautifulSoup('', 'lxml-xml')
+    soup = bs4.BeautifulSoup("", "lxml-xml")
     doctype = bs4.Doctype.for_name_and_ids(
-        'ncx',
-        '-//NISO//DTD ncx 2005-1//EN',
-        'http://www.daisy.org/z3986/2005/ncx-2005-1.dtd'
+        "ncx",
+        "-//NISO//DTD ncx 2005-1//EN",
+        "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd"
     )
     soup.append(doctype)
     ncx = soup.new_tag(
-        'ncx',
+        "ncx",
         xmlns="http://www.daisy.org/z3986/2005/ncx/",
         version="2005-1"
     )
@@ -100,13 +100,13 @@ def create_ncx(book: Book, uuid: str) -> Tuple[str, str]:
     ncx.append(create_ncx_head(soup, uuid))
     ncx.append(create_ncx_title(soup, book.metadata))
     ncx.append(create_ncx_nav_map(soup, book.chapters))
-    return 'OEBPS/toc.ncx', str(soup)
+    return "OEBPS/toc.ncx", str(soup)
 
 def create_ncx_head(
         soup: bs4.BeautifulSoup,
         uuid: str,
 ) -> bs4.element.Tag:
-    head = soup.new_tag('head')
+    head = soup.new_tag("head")
     dtb_meta = [
         ("uid", "urn:uuid:" + uuid),
         ("depth", "1"),
@@ -114,9 +114,9 @@ def create_ncx_head(
         ("maxPageNumber", "0"),
     ]
     for meta_name, meta_content in dtb_meta:
-        meta = soup.new_tag('meta')
-        meta['name'] = 'dtb:' + meta_name
-        meta['content'] = meta_content
+        meta = soup.new_tag("meta")
+        meta["name"] = "dtb:" + meta_name
+        meta["content"] = meta_content
         head.append(meta)
     return head
 
@@ -124,8 +124,8 @@ def create_ncx_title(
         soup: bs4.BeautifulSoup,
         metadata: Metadata,
 ) -> bs4.element.Tag:
-    title = soup.new_tag('docTitle')
-    text = soup.new_tag('text')
+    title = soup.new_tag("docTitle")
+    text = soup.new_tag("text")
     text.append(metadata.title)
     title.append(text)
     return title
@@ -134,7 +134,7 @@ def create_ncx_nav_map(
         soup: bs4.BeautifulSoup,
         chapters: Iterable[Chapter],
 ) -> bs4.element.Tag:
-    nav_map = soup.new_tag('navMap')
+    nav_map = soup.new_tag("navMap")
     nav_number = 1
     for chapter in chapters:
         nav_point = create_ncx_nav_point(
@@ -145,14 +145,14 @@ def create_ncx_nav_map(
         )
         nav_map.append(nav_point)
         nav_number += 1
-        chapter_soup = bs4.BeautifulSoup(chapter.xhtml, 'lxml')
-        titles = chapter_soup.find_all('h2')
+        chapter_soup = bs4.BeautifulSoup(chapter.xhtml, "lxml")
+        titles = chapter_soup.find_all("h2")
         for title in titles:
             sub_nav_point = create_ncx_nav_point(
                 soup,
                 nav_number,
                 title.text,
-                chapter.outname + '#' + title.previous_sibling.a['id'],
+                chapter.outname + "#" + title.previous_sibling.a["id"],
             )
             nav_point.append(sub_nav_point)
             nav_number += 1
@@ -176,109 +176,109 @@ def create_ncx_nav_point(
     return nav_point
 
 def create_opf(book: Book, uuid: str) -> Tuple[str, str]:
-    soup = bs4.BeautifulSoup('', 'lxml-xml')
+    soup = bs4.BeautifulSoup("", "lxml-xml")
     package_attrs = {
-        'xmlns': "http://www.idpf.org/2007/opf",
-        'xmlns:dc': "http://purl.org/dc/elements/1.1/",
-        'unique-identifier': "bookid",
-        'version': "2.0",
+        "xmlns": "http://www.idpf.org/2007/opf",
+        "xmlns:dc": "http://purl.org/dc/elements/1.1/",
+        "unique-identifier": "bookid",
+        "version": "2.0",
     }
-    package = soup.new_tag('package', **package_attrs)
+    package = soup.new_tag("package", **package_attrs)
     soup.append(package)
 
     metadata = create_opf_metadata(soup, book.metadata, uuid)
     manifest = begin_opf_manifest(soup)
-    spine = soup.new_tag('spine', toc="ncx")
+    spine = soup.new_tag("spine", toc="ncx")
     for chapter in book.chapters:
         file_id = os.path.splitext(chapter.outname)[0]
         item_attrs = {
-            'id': file_id,
-            'href': chapter.outname,
-            'media-type': "application/xhtml+xml",
+            "id": file_id,
+            "href": chapter.outname,
+            "media-type": "application/xhtml+xml",
         }
-        item = soup.new_tag('item', **item_attrs)
+        item = soup.new_tag("item", **item_attrs)
         manifest.append(item)
         ref_attrs = {
-            'idref': file_id,
+            "idref": file_id,
         }
-        ref = soup.new_tag('itemref', **ref_attrs)
+        ref = soup.new_tag("itemref", **ref_attrs)
         spine.append(ref)
 
     for element in metadata, manifest, spine:
         package.append(element)
-    return 'OEBPS/content.opf', str(soup)
+    return "OEBPS/content.opf", str(soup)
 
 def create_opf_metadata(
         soup: bs4.BeautifulSoup,
         metadata: Metadata,
         uuid: str,
 ) -> bs4.element.Tag:
-    opf_metadata = soup.new_tag('metadata')
-    title = soup.new_tag('dc:title')
+    opf_metadata = soup.new_tag("metadata")
+    title = soup.new_tag("dc:title")
     title.append(metadata.title)
-    creator = soup.new_tag('dc:creator')
+    creator = soup.new_tag("dc:creator")
     creator.append(metadata.author)
-    identifier = soup.new_tag('dc:identifier')
-    identifier['id'] = "bookid"
+    identifier = soup.new_tag("dc:identifier")
+    identifier["id"] = "bookid"
     identifier.append(uuid)
-    language = soup.new_tag('dc:language')
-    language.append('en-US')
+    language = soup.new_tag("dc:language")
+    language.append("en-US")
     for element in title, creator, identifier, language:
         opf_metadata.append(element)
     return opf_metadata
 
 def begin_opf_manifest(soup: bs4.BeautifulSoup) -> bs4.element.Tag:
-    manifest = soup.new_tag('manifest')
+    manifest = soup.new_tag("manifest")
     item_ncx_attrs = {
-        'id': "ncx",
-        'href': "toc.ncx",
-        'media-type': "application/x-dtbncx+xml",
+        "id": "ncx",
+        "href": "toc.ncx",
+        "media-type": "application/x-dtbncx+xml",
     }
-    item_ncx = soup.new_tag('item', **item_ncx_attrs)
+    item_ncx = soup.new_tag("item", **item_ncx_attrs)
     manifest.append(item_ncx)
     return manifest
 
 def create_mime() -> Tuple[str, str]:
-    return 'mimetype', 'application/epub+zip'
+    return "mimetype", "application/epub+zip"
 
 def create_container() -> Tuple[str, str]:
-    soup = bs4.BeautifulSoup('', 'lxml-xml')
+    soup = bs4.BeautifulSoup("", "lxml-xml")
     container_attrs = {
-        'xmlns': "urn:oasis:names:tc:opendocument:xmlns:container",
-        'version': "1.0",
+        "xmlns": "urn:oasis:names:tc:opendocument:xmlns:container",
+        "version": "1.0",
     }
-    container = soup.new_tag('container', **container_attrs)
-    rootfiles = soup.new_tag('rootfiles')
+    container = soup.new_tag("container", **container_attrs)
+    rootfiles = soup.new_tag("rootfiles")
     rootfile_attrs = {
-        'full-path': "OEBPS/content.opf",
-        'media-type': "application/oebps-package+xml",
+        "full-path": "OEBPS/content.opf",
+        "media-type": "application/oebps-package+xml",
     }
-    rootfile = soup.new_tag('rootfile', **rootfile_attrs)
+    rootfile = soup.new_tag("rootfile", **rootfile_attrs)
     rootfiles.append(rootfile)
     container.append(rootfiles)
     soup.append(container)
-    return 'META-INF/container.xml', str(soup)
+    return "META-INF/container.xml", str(soup)
 
 def main() -> None:
-    guide_url = 'http://zsh.sourceforge.net/Guide/'
-    tarball_url = 'http://zsh.sourceforge.net/Guide/zshguide_html.tar.gz'
+    guide_url = "http://zsh.sourceforge.net/Guide/"
+    tarball_url = "http://zsh.sourceforge.net/Guide/zshguide_html.tar.gz"
     archive = urllib.request.urlopen(tarball_url).read()
     book = book_from_tar_archive(archive)
-    epub_contents = [('OEBPS/' + c.outname, c.xhtml) for c in book.chapters]
+    epub_contents = [("OEBPS/" + c.outname, c.xhtml) for c in book.chapters]
     epub_contents.append(create_ncx(book, guide_url))
     epub_contents.append(create_opf(book, guide_url))
     # the first file in the archive must be the mimetype file
     epub_contents.insert(0, create_mime())
     epub_contents.append(create_container())
 
-    epub = zipfile.ZipFile('zsh-guide.epub', 'w')
+    epub = zipfile.ZipFile("zsh-guide.epub", "w")
     for filename, contents in epub_contents:
-        compress = filename != 'mimetype'
+        compress = filename != "mimetype"
         compression_type = zipfile.ZIP_DEFLATED if compress else zipfile.ZIP_STORED
         # the mimetype file must not be compressed; this allows non-ZIP
         # utilities to discover the mimetype by reading the raw bytes
         # from the EPUB file
-        epub.writestr(filename, contents.encode('utf-8'), compression_type)
+        epub.writestr(filename, contents.encode("utf-8"), compression_type)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
